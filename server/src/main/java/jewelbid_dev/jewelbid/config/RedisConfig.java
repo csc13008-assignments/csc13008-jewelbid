94561709d67f4bcb7c218065db051e0df8e2a33b
ntkwan <npquan2704@gmail.com>
2025-10-31 02:22:16 +0700

    fix: redis connection

--- server/src/main/java/jewelbid_dev/jewelbid/config/RedisConfig.java
+++ server/src/main/java/jewelbid_dev/jewelbid/config/RedisConfig.java
@@ -1,11 +1,8 @@
 package jewelbid_dev.jewelbid.config;
 
-import io.lettuce.core.ClientOptions;
-import io.lettuce.core.SslOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -22,21 +19,5 @@ public class RedisConfig {
         template.setHashValueSerializer(new StringRedisSerializer());
         template.afterPropertiesSet();
         return template;
-    }
-
-    @Bean
-    public LettuceClientConfiguration lettuceClientConfiguration() {
-        SslOptions sslOptions = SslOptions.builder()
-                .jdkSslProvider()
-                .build();
-
-        ClientOptions clientOptions = ClientOptions.builder()
-                .sslOptions(sslOptions)
-                .build();
-
-        return LettuceClientConfiguration.builder()
-                .clientOptions(clientOptions)
-                .useSsl()
-                .build();
     }
 }
