a4c31a4cfa9500b557b051ca85e4666b656f7b0a
ntkwan <npquan2704@gmail.com>
2025-11-07 01:50:55 +0700

    refactor: change from spring boot to nestjs

---
+++ server/src/common/enums/auth.enum.ts
@@ -0,0 +1,15 @@
+export enum UserRole {
+    BIDDER = 'bidder',
+    SELLER = 'seller',
+    ADMIN = 'admin',
+}
+
+export enum AuthType {
+    OAUTH = 'oauth',
+    STD_AUTH = 'std_auth',
+}
+
+export enum AuthProvider {
+    GOOGLE = 'google',
+    LOCAL = 'local',
+}
